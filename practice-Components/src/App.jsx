0d699607c25c295e8f8464d21fc60658b691ce98
darshan <0daxx@proton.me>
2026-02-01 17:38:31 +0530

    Created Button component

--- practice-Components/src/App.jsx
+++ practice-Components/src/App.jsx
@@ -2,6 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
+import { Button } from "./pages/Button";
 import { CheckBox } from "./pages/CheckBox";
 import { ProgressBar } from "./pages/ProgressBar";
 // import { useState } from 'react';
@@ -46,12 +47,14 @@ function App() {
   }
 
   
+
   return (
     <>
       {/* <Timer /> */}
       {/* <UserInput /> */}
       <Clock />
-      <CheckBox />
+      {/* <CheckBox /> */}
+      < Button/>
       {/* <h1>hello </h1> */}
       {/* <ProgressBar /> */}
     </>
---
+++ practice-Components/src/pages/Button.jsx
@@ -0,0 +1,13 @@
+import "./Button.css";
+
+export function Button() {
+  return (
+    <>
+      <button
+        className="btn"
+        >
+        Click me
+      </button>
+    </>
+  );
+}
